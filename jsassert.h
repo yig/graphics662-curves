4865afdfa4c81a3d956be322c0f89e11cbbc9c98
Yotam Gingold <yotam@yotamgingold.com>
2018-01-29 22:50:46 -0500

    transitioning to emscripten

--- jsassert.h
+++ jsassert.h
@@ -4,15 +4,28 @@
 #include <string>
 #include <sstream>
 
+#include "emscripten.h"
+
+inline void jsAlert( const std::string& msg ) {
+    EM_ASM({ alert( UTF8ToString($0) ); }, msg.c_str() );
+}
+inline void jsLog( const std::string& msg ) {
+    EM_ASM({ console.log( UTF8ToString($0) ); }, msg.c_str() );
+}
+inline void jsWarn( const std::string& msg ) {
+    EM_ASM({ console.warn( UTF8ToString($0) ); }, msg.c_str() );
+}
+inline void jsError( const std::string& msg ) {
+    EM_ASM({ console.error( UTF8ToString($0) ); }, msg.c_str() );
+}
+
 #ifdef NDEBUG
     #define jsAssert( cond ) do {} while(0)
 #else
-    extern void jsAlert( const std::string& msg );
-    
     #define jsAssert( cond ) \
     do { \
         if( !(cond) ) { \
-            jsAlert( std::string("assert ") + #cond + std::string(" failed at ") + __FILE__ + std::string(":") + __LINE__ ); \
+            jsAlert( std::string("assert ") + std::string(#cond) + std::string(" failed at ") + std::string(__FILE__) + std::string(":") + toString(__LINE__) ); \
         } \
     } while(0)
 #endif
